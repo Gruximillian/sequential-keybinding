b5c0456e674e4b1d86fc9109f5a7887b6935916a
Vojislav Grujic <vojislav.grujic@gmail.com>
2018-12-18 23:49:13 +0100

    Finish basic project

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
+    let buffer = [];
+    let lastKeyTime = Date.now();
+
     document.addEventListener('keydown', event => {
+        const charList = 'abcdefghijklmnopqrstuvwxyz0123456789';
         const key = event.key.toLowerCase();
-        console.log(key);
+
+        // we are only interested in alphanumeric keys
+        if (charList.indexOf(key) === -1) return;
+
+        const currentTime = Date.now();
+
+        if (currentTime - lastKeyTime > 1000) {
+            buffer = [];
+        }
+
+        buffer.push(key);
+        lastKeyTime = currentTime;
+
+        const container = document.querySelector('#background');
+        container.style.backgroundImage = `url(images/${buffer.join('')}.jpg)`;
     });
 });
